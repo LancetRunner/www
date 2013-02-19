27498cf75f201b6d1431ac87c4f5eb46884b4069
lincolnnus <shaohuan.li@gmail.com>
2013-02-20 01:58:04 +0800

    Deleted Unnecessary files

--- bookannotation/sites/all/modules/annotation_block/annotation_block.js
+++
@@ -1,8 +0,0 @@
-(function($){
-	$(function(){
-		$('.annotation-teaser a.more-link').click(function(event) {
-			var attr = $(this).attr('id');
-			PDFView.page = attr.replace(/[^0-9]/g, '');
-        });
-	});
-})(jQuery);
