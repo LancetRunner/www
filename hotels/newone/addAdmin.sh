4adcd136338d5d487629911d0a7331bc2030edb8
lincolnnus <shaohuan.li@gmail.com>
2013-02-20 02:31:04 +0800

    Removed Unnecessary Files

--- hotels/newone/addAdmin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-$ mysql -u root -p hotel1<<EOFMYSQL
-INSERT INTO `admin` (`uid`, `fname`, `lname`, `title`, `uaddress`, `email`, `password`, `lastlogin`, `token`, `numlogin`, `type`) VALUES
-(1, 'Alan', 'Lim', 'Mr.', 'Asplan', 'lim@lim.com', 'lim', '2013-01-02 18:37:54', 'abc', 26, 1);
-INSERT INTO `hotel` (`hid`, `hname`, `haddress`, `bannerUrl`, `message`, `imageUrl`, `tac`, `contact`, `step`, `zip`) VALUES
-(1, 'Asia Sth Hotel', '25 Lower Kent Ridge Road', 'http://localhost/hotel/images/banner.png', 'some description', 'http://hotels.online.com.sg/DB/hotelpics/singapore/Pan_Pacific_Singapore_Hotel-Logo.jpg', 'Terms and Conditions', '+86123485', 4, '12345');
-EOFMYSQL
--- hotels/newone/createDB.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-printf "Step 1:Enter Database Name for the New Hotel:\n"
-read hotel
-mysql -u root -p <<EOFMYSQL
-CREATE DATABASE $hotel;
-EOFMYSQL
